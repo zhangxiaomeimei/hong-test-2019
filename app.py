afbccdeaee431b52a38d2fe577ea14333c43719c
Jen-Yen Lin <jylinor@gmail.com>
2019-03-19 21:19:20 +0800

    init

--- app.py
+++ app.py
@@ -89,20 +89,20 @@ def handle_message(event):
     # Location_Message = LocationSendMessage(title='my location', address='Tainan', latitude=22.994821, longitude=120.196452)
 
     Imagemap_Message = ImagemapSendMessage(
-        base_url='https://www.kamigo.tw/assets/kamigo-c3b10dff4cdb60fa447496b22edad6c32fffde96de20262efba690892e4461e8.png#',
+        base_url='https://i.ytimg.com/vi/sGRv8ZBLuW0/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLC6biRlKYBPBR1AeKXrlLQRYpovXQ#',
         alt_text='this is an imagemap',
-        base_size=BaseSize(height=520, width=520),
+        base_size=BaseSize(height=1040, width=1040),
         actions=[
             URIImagemapAction(
-                link_uri='https://www.kamigo.tw/',
+                link_uri='https://www.youtube.com/watch?v=sGRv8ZBLuW0',
                 area=ImagemapArea(
-                    x=0, y=0, width=260, height=520
+                    x=0, y=0, width=520, height=1040
                 )
             ),
             MessageImagemapAction(
                 text='hello',
                 area=ImagemapArea(
-                    x=520, y=0, width=260, height=520
+                    x=520, y=0, width=520, height=1040
                 )
             )
         ]
